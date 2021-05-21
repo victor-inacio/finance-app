f68634a8f33a8215f462e9367433a126ab1ee2bb
Victor Inácio <chustrupgamer@gmail.com>
2021-05-21 14:28:33 -0300

    React Navigator setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      
+      <Stack.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
